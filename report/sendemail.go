160e71fadb93c5c583733de90d7753017d9c5a90
Doug Campbell <dcampbell@gogoair.com>
2017-02-27 14:39:09 -0600

    added basic template building logic and skeleton template

--- report/sendemail.go
+++ report/sendemail.go
@@ -1,7 +1,9 @@
 package report
 
 import (
+	"html/template"
 	"log"
+	"os"
 	"time"
 
 	"github.com/ndcampbell/conformitygopher/configs"
@@ -9,11 +11,18 @@ import (
 	"gopkg.in/gomail.v2"
 )
 
+type TemplateData struct {
+	CurDate  string
+	Resource string
+}
+
 func SendEmail(config *configs.EmailConfig) {
 	curDate := time.Now().Format("2006-01-02")
 	subject := "ConformityGopher Report - " + curDate
 	d := gomail.NewDialer(config.Host, config.Port, config.Username, config.Password)
 	//Sends email to everyone in config list
+	data := TemplateData{CurDate: curDate, Resource: "ec2"}
+	buildTemplate(&data)
 	for _, to := range config.Recipients {
 		m := buildMessage(config.Sender, to, subject)
 		if err := d.DialAndSend(m); err != nil {
@@ -30,4 +39,15 @@ func buildMessage(sender string, recipient string, subject string) *gomail.Messa
 	m.SetHeader("Subject", subject)
 	m.SetBody("text/html", "Testing <b>Testing</b>")
 	return m
+}
+
+func buildTemplate(data *TemplateData) {
+	t, err := template.ParseFiles("report/templates/emailtemplate.html")
+	if err != nil {
+		log.Fatal("error loading email template: ", err)
+	}
+	err = t.Execute(os.Stdout, data)
+	if err != nil {
+		log.Fatal("error rendering template: ", err)
+	}
 }
